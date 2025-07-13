ef42a9b87c8190e74059151d2f407b6a99ef1362
Jesus Soto <sotous0@gmail.com>
2025-07-13 11:56:55 -0500

    Add session management to the signin module

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,6 +17,10 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
 
+  saveAuthTokenInSession = (token) => {
+    window.sessionStorage.setItem('token', token);
+  };
+
   onSubmitSignIn = () => {
     fetch('http://localhost:3000/signin', {
       method: 'post',
@@ -27,10 +31,21 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(user => {
-        if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+      .then(data => {
+        if (data.userId && data.success === true) {
+          this.saveAuthTokenInSession(data.token);
+          fetch('http://localhost:3000/profile/' + data.userId, {
+            method: 'get',
+            headers: {'Content-Type': 'application/json', 'Authorization': data.token}
+          })
+          .then(response => response.json())
+          .then(user => {
+            if (user && user.email) {
+              this.props.loadUser(user)
+              this.props.onRouteChange('home');
+            }
+          })
+          .catch(console.log)
         }
       })
   }
