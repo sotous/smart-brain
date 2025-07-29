21dabcbb257ba7598d3aaa40dd204ed640bc8703
Jesus Soto <sotous0@gmail.com>
2025-07-28 21:13:32 -0500

    Implement Rank in profile

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rank: 0,
+      emoji: ''
+    }
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.entries === this.props.entries && prevProps.name === this.props.name) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://ojcqv180pi.execute-api.us-east-1.amazonaws.com?rank=${entries}`)
+      .then(response => response.json())
+      .then(data => this.setState({ emoji: data.emoji }))
+      .catch(console.log);
+  }
+
+  render() {
+    const { name, entries } = this.props;
+    const { emoji} = this.state;
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
+        <div className='white f3'>
+          {`Rank Badge: ${emoji}`}
+        </div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+      );
+    }
 }
 
 export default Rank;
